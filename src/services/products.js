da64f66f31971f1f4fdf5a1ccad48005926a887a
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-10 12:47:24 +0300

    task_7_done

--- src/services/products.js
+++ src/services/products.js
@@ -1,24 +1,33 @@
-import { ProductModel } from "../db/models/product.js";
+import { ProductModel } from '../db/models/product.js';
 
 export const getProducts = async () => {
-    const products = await ProductModel.find();
+  const products = await ProductModel.find();
 
-    return products;
+  return products;
 };
 
-
 export const getProductById = async (productId) => {
-    const product = await ProductModel.findOne({ _id: productId });
-    return product;
+  const product = await ProductModel.findOne({ _id: productId });
+  return product;
 };
 
 export const createProduct = async (payload) => {
-    const product = await ProductModel.create(payload);
-    return product;
-
+  const product = await ProductModel.create(payload);
+  return product;
 };
 
 export const patchProduct = async (productId, payload) => {
-    const product = await ProductModel.findOneAndUpdate({ _id: productId }, payload, { new: true });
-    return product;
+  const product = await ProductModel.findOneAndUpdate(
+    { _id: productId },
+    payload,
+    { new: true },
+  );
+  return product;
+};
+
+export const deleteProduct = async (productId) => {
+  const product = await ProductModel.findOneAndDelete({
+    _id: productId,
+  });
+  return product;
 };
