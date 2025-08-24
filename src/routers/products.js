b09b1afde12be4455fb390b1d7513224861176c2
mafutka <mafutka@gmail.com>
2025-08-24 11:10:14 +0300

    task-1-validation-done

--- src/routers/products.js
+++ src/routers/products.js
@@ -6,13 +6,16 @@ import {
   updateProductController,
   deleteProductController,
 } from '../controllers/products.js';
+import {validateBody} from '../middlewares/validateBody.js';
+import { createProductSchema } from '../validation/product.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/products', getProductController);
-router.get('/products/:productId', getProductIdController);
-router.post('/products', createProductController);
-router.patch('/products/:productId', updateProductController);
-router.delete('/products/:productId', deleteProductController);
+router.get('/products/:productId', isValidId, getProductIdController);
+router.post('/products', validateBody(createProductSchema), createProductController);
+router.patch('/products/:productId', isValidId, validateBody(createProductSchema), updateProductController);
+router.delete('/products/:productId', isValidId, deleteProductController);
 
 export default router;
